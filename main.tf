1d62c664cdf5755a331e827866abc3dac97da234
Vinith Amma <vinithamma007@gmail.com>
2025-03-19 22:29:32 +0530

    tf practice

--- main.tf
+++ main.tf
@@ -17,25 +17,3 @@ resource "aws_lb" "test" {
     Environment = "production"
   }
 }
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 22
-    protocol    = "tcp"
-    to_port     = 22
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    protocol    = "-1"
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "allow_tls"
-  }
-}
--- vars.tf
+++ vars.tf
@@ -1,6 +1,5 @@
 variable "env" {}
 variable "tags" {}
-variable "alb" {}
 variable "lb_type" {}
 variable "internal" {}
-variable "sg_ingress_cidr" {}
+variable "vpc_id" {}
